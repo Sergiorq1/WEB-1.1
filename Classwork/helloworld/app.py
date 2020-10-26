3d7a22918a693e1d94e4e594195cfa253d00bc86
Sergiorq1 <66581064+Sergiorq1@users.noreply.github.com>
2020-10-26 15:02:38 -0700

    Update app.py

--- Classwork/helloworld/app.py
+++ Classwork/helloworld/app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def homepage():
     return "Hello, world!"
 
+@app.route('/profile/<users_name>')
+def profile(users_name):
+    return "Hello " + users_name
+    
 if __name__ == '__main__':
     app.run(debug=True)
